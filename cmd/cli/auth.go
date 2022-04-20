3d9db6f259d0aaa3d3ba32fca27a3da59972ffbb
Andres Osorio <af.osorio1341@gmail.com>
2022-04-20 16:31:57 -0500

    Added auth middleware templates when running the make auth command

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 func doAuth() error {
@@ -18,7 +20,6 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
-
 	err = copyDataToFile([]byte("drop table if exists users cascade; drop table if exists tokens cascade; drop table if exists remember_tokens;"), downFile)
 	if err != nil {
 		exitGracefully(err)
@@ -33,11 +34,25 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
-
 	err = copyFileFromTemplate("templates/data/token.go.txt", scap.RootPath+"/data/token.go")
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	err = copyFileFromTemplate("templates/middleware/auth.go.txt", scap.RootPath+"/middleware/auth.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+	err = copyFileFromTemplate("templates/middleware/auth-token.go.txt", scap.RootPath+"/middleware/auth-token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	color.Yellow(" - users, tokens, and remember_tokens migrations created and executed")
+	color.Yellow(" - user and token models create")
+	color.Yellow(" - auth middleware created")
+	color.Yellow("")
+	color.Yellow("Don't forget to add user and token models in data/models.go, and to add appropriate middleware to your routes!")
 
 	return nil
 }
